73f9fd80739d3fbcbcb7dad00b46198a308fe013
ThatEpicBanana <thatepicbanana132@gmail.com>
2023-05-22 10:52:24 -0400

    unspeakable things

---
+++ src/calendar/drawing/layers/Layer.java
@@ -0,0 +1,35 @@
+package calendar.drawing.layers;
+
+public class Layer {
+    // 2D array of all of the chars in the layer
+    // column arrays!
+    private char[][] chars;
+
+    private int width;
+    private int height;
+
+    public Layer(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        this.chars = new char[width][height];
+        this.fill(' ');
+    }
+
+    private void fill(char with) {
+        for(int i = 0; i < width; i++)
+            for(int j = 0; j < height; j++)
+                this.chars[i][j] = with;
+    }
+
+    public void print() {
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++)
+                System.out.print(this.chars[i][j]);
+            System.out.println();
+        }
+    }
+
+    public int width() { return width; }
+    public int height() { return height; }
+}
