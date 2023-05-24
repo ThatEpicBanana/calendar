dc75745ee27dbddda33ef2296417eaa1b6d818d1
Mocha <71618230+Mochaccinoo@users.noreply.github.com>
2023-05-24 12:16:11 -0400

    Fixed issues where 'section' would not resolve

--- src/calendar/storage/Calendar.java
+++ src/calendar/storage/Calendar.java
@@ -114,3 +114,29 @@ public class Calendar {
         calendar.display();
     }
 }
+
+class Section {
+    private String title;
+    private String color;
+
+    public Section(String title, String color) {
+        this.title = title;
+        this.color = color;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
