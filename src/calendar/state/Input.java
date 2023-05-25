f176eef737626f9bf277e9aa934e620485d9906f
ThatEpicBanana <thatepicbanana132@gmail.com>
2023-05-25 01:03:54 -0400

    outline some things

---
+++ src/calendar/state/Input.java
@@ -0,0 +1,3 @@
+
+// handles input in some way idk
+// might have different input classes based on each screen
---
+++ src/calendar/state/State.java
@@ -0,0 +1,4 @@
+
+// handles the state of the app
+// what screen is showing, actually storing a Calendar object, etc
+// plays nicely with Input
