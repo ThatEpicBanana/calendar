08cfe59b761b77d59afa09a9f01496ed2acbe43c
Mocha <71618230+Mochaccinoo@users.noreply.github.com>
2023-05-25 14:59:13 -0400

    removed a thing i think

--- src/calendar/state/Input.java
+++ src/calendar/state/Input.java
@@ -1,3 +1,7 @@
 
 // handles input in some way idk
 // might have different input classes based on each screen
+
+// store which screen we're on
+// any popups that are on
+// interaction with the input
